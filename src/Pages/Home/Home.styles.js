ba1755e6c77ff2877ab456bb0722a8dbdb4a5345
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-07-31 19:37:08 -0300

    add home styles

---
+++ src/Pages/Home/Home.styles.js
@@ -0,0 +1,26 @@
+import { styled } from "styled-components";
+
+export const HomeHeaderWrapper = styled.nav`
+  display: flex;
+  flex-direction: column;
+  margin-top: 140px;
+  align-items: center;
+`
+
+export const HomeTitle = styled.h1`
+  font-size: 48px;
+  margin: 0;
+  font-weight: 900;
+  line-height: normal;
+  color: var(--black);
+  text-align: center;
+  `
+  
+  export const HeaderText = styled.p`
+  text-align: center;
+  margin: 0;
+  font-size: 24px;
+  font-weight: 500;
+  color: var(--black);
+  opacity: .6;
+`
