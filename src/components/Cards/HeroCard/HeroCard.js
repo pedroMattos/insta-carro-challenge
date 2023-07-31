401f8ba978e0369f26b77cc2bf3e147fae33987e
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-07-31 20:36:15 -0300

    add hero card

---
+++ src/components/Cards/HeroCard/HeroCard.js
@@ -0,0 +1,9 @@
+import { HeroCardWrapper } from "./HeroCard.styles";
+
+export default function HeroCard({ heroData }) {
+  return (
+    <HeroCardWrapper>
+      <p>card</p>
+    </HeroCardWrapper>
+  )
+}
---
+++ src/components/Cards/HeroCard/HeroCard.styles.js
@@ -0,0 +1,10 @@
+import { styled } from "styled-components";
+
+export const HeroCardWrapper = styled.div`
+  display: flex;
+  flex-direction: colunm;
+  width: 100%;
+  height: 240px;
+  border: 1px solid var(--gray);
+  border-radius: 10px;
+`
