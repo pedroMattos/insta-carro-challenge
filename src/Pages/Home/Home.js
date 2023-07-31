76b9a73573bac9431850ce4bcff869d9106f488f
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-07-31 20:36:27 -0300

    add home cards component

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,11 @@
+import HomeCards from "./HomeCards";
 import HomeHeader from "./HomeHeader";
 
 export default function Home() {
   return (
     <>
       <HomeHeader />
+      <HomeCards />
     </>
   )
 }
