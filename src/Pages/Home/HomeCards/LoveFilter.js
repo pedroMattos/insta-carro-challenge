96ebddeaaaaadfa93d3f8c4bb4fe5a04d5b696ce
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-07-31 20:37:33 -0300

    love filter button

---
+++ src/Pages/Home/HomeCards/LoveFilter.js
@@ -0,0 +1,6 @@
+import LoveIcon from "../../../components/Icons/LoveIcon/LoveIcon";
+import { LoveFilterButton } from "../Home.styles";
+
+export default function LoveFilter() {
+  return (<LoveFilterButton><LoveIcon width={20} height={20} />Somente favoritos</LoveFilterButton>)
+}
