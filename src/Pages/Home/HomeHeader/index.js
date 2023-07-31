6f13bfe4beda903f4bda5135c72d870be03a4fbc
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-07-31 19:37:17 -0300

    add home header

---
+++ src/Pages/Home/HomeHeader/index.js
@@ -0,0 +1,15 @@
+import SearchField from "../../../components/FormFields/SearchField/SearchField";
+import { HeaderText, HomeHeaderWrapper, HomeTitle } from "../Home.styles";
+
+export default function HomeHeader() {
+  return (
+    <HomeHeaderWrapper>
+      <HomeTitle>Explore o universo e crie a sua equipe</HomeTitle>
+      <HeaderText>
+        Os melhores personagens já feitos em quadrinhos.
+        Fique viciado em uma generosa porção de heróis e vilões!
+      </HeaderText>
+      <SearchField />
+    </HomeHeaderWrapper>
+  )
+}
