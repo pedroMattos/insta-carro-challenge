da1b4425b359ed734d21f43268cee8c01534a6e3
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-08-01 10:04:15 -0300

    add component styles

--- src/components/Cards/HeroCard/HeroCard.styles.js
+++ src/components/Cards/HeroCard/HeroCard.styles.js
@@ -4,7 +4,7 @@ export const HeroCardWrapper = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  height: 240px;
+  height: 250px;
   border: 1px solid var(--gray);
   border-radius: 10px;
   max-width: 206px;
@@ -30,6 +30,7 @@ export const HeroLoveAndName = styled.div`
   width: 100%;
   display: flex;
   justify-content: space-between;
+  align-items: center;
 `
 
 export const HeroName = styled.p`
@@ -43,4 +44,35 @@ export const HeroName = styled.p`
 
 export const LoveButton = styled.button`
   margin-right: 13px;
+  background-color: white;
+  border: 0;
+  cursor: pointer;
+  border-radius: 50%;
+  height: 30px;
+  width: 30px;
+  padding: 3px;
+  transition-duration: .5s;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  &:hover {
+    svg path {
+      fill: white;
+    }
+    transition-duration: .5s;
+    background-color: var(--orange);
+    opacity: .8;
+  }
+`
+
+export const HeroDescription = styled.p`
+  margin: 0;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  font-size: 14px;
+  width: 180px;
+  color: var(--black);
 `
