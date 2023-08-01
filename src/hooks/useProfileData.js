4497e9961ba42eb970b51c9c910ff6dd77784ea9
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-08-01 20:12:44 -0300

    rename states and hook

--- src/hooks/useProfileData.js
+++ src/hooks/useProfileData.js
@@ -3,21 +3,21 @@ import getHeroById from "../services/getHeroById";
 import { useParams } from "react-router-dom";
 
 export default function useProfileData() {
-  const { id, name } = useParams()
-  const [heroData, setHeroData] = useState(null)
+  const { id } = useParams()
+  const [comicsData, setComisData] = useState(null)
   const [isLoading, setIsLoading] = useState(true)
   const [bannerImage, setBannerImage] = useState(null)
 
-  const fetchHeroData = async () => {
+  const fetchComics = async () => {
     const { results } = await getHeroById(id)
-    setHeroData(results)
+    setComisData(results)
     setBannerImage(`${results.at(0).images.at(0).path}.${results.at(0).images.at(0).extension}`)
     setIsLoading(false)
   }
 
   useEffect(() => {
-    fetchHeroData()
+    fetchComics()
   }, [])
 
-  return { isLoading, heroData, name, bannerImage }
+  return { isLoading, comicsData, bannerImage }
 }
