8201b0b014f998602f23515ce722cb563f74e0cc
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-08-01 17:44:33 -0300

    add load spinner

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,5 @@
+import { LoadingContainer, LoadingSpinner } from "./Loading.styles";
+
+export default function Loading() {
+  return(<LoadingContainer><LoadingSpinner /></LoadingContainer>)
+}
---
+++ src/components/Loading/Loading.styles.js
@@ -0,0 +1,21 @@
+import { styled } from "styled-components";
+
+export const LoadingContainer = styled.div`
+  width: 100px;
+  height: 100px;
+  margin: auto;
+`
+
+export const LoadingSpinner = styled.div`
+  width: 40px;
+  height: 40px;
+  border-left: 1px solid var(--orange);
+  border-right: 1px solid var(--orange);
+  border-radius: 50%;
+  animation: SpinnerMove 1.2s infinite;
+  
+  @keyframes SpinnerMove {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+  }
+`
