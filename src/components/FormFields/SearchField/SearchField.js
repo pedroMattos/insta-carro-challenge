7a83f892d4f007617b7f33aa494fb7699f032940
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-08-01 17:53:24 -0300

    change icon domension

--- src/components/FormFields/SearchField/SearchField.js
+++ src/components/FormFields/SearchField/SearchField.js
@@ -4,7 +4,7 @@ import { CustomField, InputWrapper } from "./SearchField.styles";
 export default function SearchField({ onChange }) {
   return (
     <InputWrapper>
-      <SearchIcon width={30} height={30} />
+      <SearchIcon width={20} height={20} />
       <CustomField type="text" placeholder="Procure por heróis" onChange={onChange} />
     </InputWrapper>
   )
