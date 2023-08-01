a5d2d71af40a774c0f7753388e93cb6ec5dfec12
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-08-01 19:44:35 -0300

    add mapstate props

--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -1,16 +1,24 @@
+import { connect } from "react-redux";
 import HeroDetailCard from "../../components/Cards/HeroDetailCard/HeroDetailCard";
 import Footer from "../../components/Footer/Footer";
 import useProfileData from "../../hooks/useProfileData";
 import ProfileHeader from "./ProfileHeader";
 
-export default function Profile() {
+const mapStateToProps = (state) => {
+  return {
+    data: state.data.data,
+  };
+}
+
+function Profile({ data }) {
   const { heroData, isLoading, name, bannerImage } = useProfileData()
-  console.log(heroData, name)
   return (
     <>
       <ProfileHeader image={bannerImage} />
-      <HeroDetailCard/>
+      <HeroDetailCard heroData={data}/>
       <Footer />
     </>
   )
 }
+
+export default connect(mapStateToProps)(Profile)
