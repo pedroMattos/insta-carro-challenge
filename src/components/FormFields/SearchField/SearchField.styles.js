db61c2e1ded05cce34929f9a284fab1bd409c37c
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-08-01 17:53:40 -0300

    fix search bar styles

--- src/components/FormFields/SearchField/SearchField.styles.js
+++ src/components/FormFields/SearchField/SearchField.styles.js
@@ -4,10 +4,10 @@ export const CustomField = styled.input`
   width: 680px;
   border-radius: 100px;
   border: 0;
-  background-color: var(--orange-100);
-  opacity: .2;
-  padding: 20px 100px;
-  font-size: 24px;
+  background-color: var(--orange-100-opacity);
+  padding: 10px 100px;
+  font-size: 18px;
+  color: var(--orange);
   &::placeholder {
     color: var(--orange);
   }
@@ -15,10 +15,10 @@ export const CustomField = styled.input`
 
 export const InputWrapper = styled.div`
   position: relative;
-  margin-top: 50px;
+  margin-bottom: 30px;
   svg {
     position: absolute;
-    top: 18px;
+    top: 10px;
     left: 40px;
   }
 `
