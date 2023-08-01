e8d0fb7d0a28bd7288bee4443dc1f7fcb5517031
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-07-31 21:25:13 -0300

    add hero card component and styles

--- src/components/Cards/HeroCard/HeroCard.js
+++ src/components/Cards/HeroCard/HeroCard.js
@@ -1,9 +1,18 @@
-import { HeroCardWrapper } from "./HeroCard.styles";
+import LoveIconOff from "../../Icons/LoveIconOff/LoveIconOff";
+import { HeroCardWrapper, HeroFigure, HeroLoveAndName, HeroName, LoveButton } from "./HeroCard.styles";
 
 export default function HeroCard({ heroData }) {
   return (
     <HeroCardWrapper>
-      <p>card</p>
+      <HeroFigure>
+        <img src={`${heroData.thumbnail.path}.${heroData.thumbnail.extension}`} alt={heroData.name} />
+      </HeroFigure>
+      <HeroLoveAndName>
+        <HeroName>{heroData.name}</HeroName>
+        <LoveButton>
+          <LoveIconOff width={20} height={20} />
+        </LoveButton>
+      </HeroLoveAndName>
     </HeroCardWrapper>
   )
 }
--- src/components/Cards/HeroCard/HeroCard.styles.js
+++ src/components/Cards/HeroCard/HeroCard.styles.js
@@ -2,9 +2,45 @@ import { styled } from "styled-components";
 
 export const HeroCardWrapper = styled.div`
   display: flex;
-  flex-direction: colunm;
+  flex-direction: column;
   width: 100%;
   height: 240px;
   border: 1px solid var(--gray);
   border-radius: 10px;
+  max-width: 206px;
+  align-items: center;
+  padding-top: 10px;
+  gap: 10px;
+`
+
+export const HeroFigure = styled.figure`
+  width: 180px;
+  height: 130px;
+  margin: 0;
+  overflow: hidden;
+  border-radius: 8px;
+  img {
+    width: 100%;
+    height: auto;
+    object-fit: cover;
+  }
+`
+
+export const HeroLoveAndName = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`
+
+export const HeroName = styled.p`
+  margin: 0;
+  width: 100px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  padding-left: 13px;
+`
+
+export const LoveButton = styled.button`
+  margin-right: 13px;
 `
