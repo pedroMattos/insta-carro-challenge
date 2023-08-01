f3c2ee7439794279cdc1c438f55c84aabf4c0402
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-08-01 14:48:02 -0300

    add navigation to profile page

--- src/components/Cards/HeroCard/HeroCard.js
+++ src/components/Cards/HeroCard/HeroCard.js
@@ -1,20 +1,25 @@
+import { useNavigate } from "react-router-dom";
 import LoveIconOff from "../../Icons/LoveIconOff/LoveIconOff";
 import { HeroCardWrapper, HeroDescription, HeroFigure, HeroLoveAndName, HeroName, LoveButton } from "./HeroCard.styles";
 
 export default function HeroCard({ heroData }) {
+  const navigate = useNavigate()
   const heroDescriptionText = !!heroData.description.trim() ? heroData.description : 'Herói sem descrição'
+  const handleNavigate = () => {
+    navigate(`/profile/${heroData.id}`)
+  }
   return (
     <HeroCardWrapper>
-      <HeroFigure>
+      <HeroFigure onClick={handleNavigate}>
         <img src={`${heroData.thumbnail.path}.${heroData.thumbnail.extension}`} alt={heroData.name} />
       </HeroFigure>
       <HeroLoveAndName>
-        <HeroName>{heroData.name}</HeroName>
+        <HeroName onClick={handleNavigate}>{heroData.name}</HeroName>
         <LoveButton>
           <LoveIconOff width={20} height={20} />
         </LoveButton>
       </HeroLoveAndName>
-      <HeroDescription>
+      <HeroDescription onClick={handleNavigate}>
         {heroDescriptionText}
       </HeroDescription>
     </HeroCardWrapper>
