6a8b8db109c8f3daaf12b7d7555146679b326bf9
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-08-01 18:59:14 -0300

    add hero card detail

---
+++ src/components/Cards/HeroDetailCard/HeroDetailCard.js
@@ -0,0 +1,9 @@
+import { HeroDetailCardWrapper } from "./HeroDetailCard.styles";
+
+export default function HeroDetailCard ({ heroData }) {
+  return(
+    <HeroDetailCardWrapper>
+      <p>ttt</p>
+    </HeroDetailCardWrapper>
+  )
+}
---
+++ src/components/Cards/HeroDetailCard/HeroDetailCard.styles.js
@@ -0,0 +1,14 @@
+import { styled } from "styled-components";
+
+export const HeroDetailCardWrapper = styled.div`
+  width: 80%;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  border-radius: 40px;
+  height: 200px;
+  border: 1px solid red;
+  margin: 0 auto;
+  position: relative;
+  bottom: 100px;
+  z-index: 5;
+  background-color: white;
+`
