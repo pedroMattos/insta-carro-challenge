7ab05cb3e822673d862c09cf4cf09aa1c0478baa
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-08-01 21:31:28 -0300

    add load state

--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -3,6 +3,8 @@ import HeroDetailCard from "../../components/Cards/HeroDetailCard/HeroDetailCard
 import Footer from "../../components/Footer/Footer";
 import useProfileData from "../../hooks/useProfileData";
 import ProfileHeader from "./ProfileHeader";
+import ComicCardsArea from "./ComicCardsArea";
+import Loading from "../../components/Loading/Loading";
 
 const mapStateToProps = (state) => {
   return {
@@ -16,6 +18,9 @@ function Profile({ data }) {
     <>
       <ProfileHeader image={bannerImage} />
       <HeroDetailCard heroData={data}/>
+      {!isLoading ?
+      (<ComicCardsArea comicsData={comicsData} />)  
+      : <Loading />}
       <Footer />
     </>
   )
