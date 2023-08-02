1a66bf4e841be5943a167054a8dbede2a2ead203
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-08-02 00:36:17 -0300

    add on click event

--- src/Pages/Home/HomeCards/LoveFilter.js
+++ src/Pages/Home/HomeCards/LoveFilter.js
@@ -1,6 +1,6 @@
 import LoveIcon from "../../../components/Icons/LoveIcon/LoveIcon";
 import { LoveFilterButton } from "../Home.styles";
 
-export default function LoveFilter() {
-  return (<LoveFilterButton><LoveIcon width={20} height={20} />Somente favoritos</LoveFilterButton>)
+export default function LoveFilter({ onClick }) {
+  return (<LoveFilterButton onClick={onClick}><LoveIcon width={20} height={20} />Somente favoritos</LoveFilterButton>)
 }
