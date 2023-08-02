972bf350676822af07287ccdb7bec15956573298
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-08-02 00:36:02 -0300

    add component to show heroes

---
+++ src/Pages/Home/HomeCards/AllHeroes.js
@@ -0,0 +1,9 @@
+import HeroCard from "../../../components/Cards/HeroCard/HeroCard";
+import Loading from "../../../components/Loading/Loading";
+
+export default function AllHeroes({ showFavorites, isLoading, heroesData, favoriteHeroes }) {
+
+  if (isLoading) return <Loading />
+  if (showFavorites) return favoriteHeroes.map((hero) => <HeroCard key={hero.id} heroData={hero} />)
+  return (heroesData.map((hero) => <HeroCard key={hero.id} heroData={hero} />))
+}
