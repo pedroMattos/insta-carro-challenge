277d6e8cd8daf8620ec9b868b31b3ada18be5ccc
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-08-02 10:43:28 -0300

    add persist gate

--- src/index.js
+++ src/index.js
@@ -7,15 +7,16 @@ import {
 } from "react-router-dom";
 import Home from './Pages/Home/Home';
 import Profile from './Pages/Profile/Profile';
-import store from './store/store'
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import store from './store/index.js';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: (
     <React.StrictMode>
-      <Provider store={store}>
+      <Provider store={store.store}>
         <Home />
       </Provider>
     </React.StrictMode>
@@ -25,8 +26,10 @@ const router = createBrowserRouter([
     path: "/profile/:id",
     element: (
     <React.StrictMode>
-      <Provider store={store}>
+      <Provider store={store.store}>
+      <PersistGate loading={null} persistor={store.persistor}>
         <Profile />
+      </PersistGate>
       </Provider>
     </React.StrictMode>
     ),
